5035fc5b34c102a3870329806240a1ba5d36614b
Roshan <itsroshanksingh@gmail.com>
2023-12-05 15:20:53 -0500

    remove cleanup

--- install_r_library.R
+++ install_r_library.R
@@ -3,6 +3,6 @@ options(repos = c(CRAN = "https://cran.r-project.org"))
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
 }
-
+install.packages("GenomeInfoDb")
 BiocManager::install("seqLogo")
 BiocManager::install("Biostrings")
